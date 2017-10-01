33773e0797bc4e6dfd6fb2f48f2ac2c090fa15b7
Alex Campbell <chalecampb@gmail.com>
2017-10-01 02:25:36 -0400

    Finished updating elements.py and isotopes.py to use elementdatabase.py

--- plasmapy/atomic/elementdatabase.py
+++ plasmapy/atomic/elementdatabase.py
@@ -11,7 +11,7 @@ class ElementDatabase(object):
 	# Proton to Electron Mass Ratio
 	data = None		
 	
-	def __init__(self, filename="elemental_default.json", validate=False,
+	def __init__(self, filename="element_default.json", validate=False,
 				 use_isotope_name=True):
 		"""
 		Parameters
@@ -36,16 +36,62 @@ class ElementDatabase(object):
 				try:
 					self.data = json.load(infile)
 				except Exception as e:
-					print("Error accessing elemental JSON database (%s)" % e)
+					raise Exception("Error accessing element JSON database (%s)" % e)
 		except Exception as e:
-			print("Error accessing elemental JSON database filename (%s)" % filename)
+			raise Exception("Error accessing element JSON database filename (%s)" % filename)
 					
 		if validate:
 			self.validate()
-					
-	def write_database(self, filename, pretty=False):		
-		pass		 
+			
+	def __getitem__(self, key):
+		# Determine if key is an integer, a symbol, a name, or an 
+		# isotope (denoted by name or symbol appended with -#
+		if isinstance(key, int):
+			# It's an integer, return the element corresponding to
+			# that atomic number. 			
+			for i in self.data["data"]:
+				if i["atomic_number"] == key:
+					return i
 		
+		# Check if it's an isotope
+		if "-" in key:
+			symb, mass_number = key.split("-")
+			
+			# Check if it's a symbol or a name			
+			if len(symb) < 3:				
+				# It's a symbol
+				candidates = [x for x in self.data["data"] if x["symbol"] == symb]
+			else:
+				# It's a name
+				candidates = [x for x in self.data["data"] if x["name"].lower() == symb.lower()]
+						
+			# Find the mass number			
+			candidates = [x for x in candidates if x["mass_number"] == int(mass_number)]			
+			
+			if candidates:
+				return candidates[0]
+			else:
+				raise KeyError("Key %s was not found in the database." % key)
+				
+		# Finally if it's a name or a symbol, just find it without
+		# parsing the mass number (handle D and T which are noncompliant)
+		candidates = [x for x in self.data["data"] if (not x["is_isotope"] 
+		                                               or x["symbol"] in ["D", "T"])]
+		
+		if len(key) < 3:
+			candidates = [x for x in candidates if x["symbol"] == key]
+		else:
+			candidates = [x for x in candidates if x["name"].lower() == key.lower()]
+		
+		if candidates:
+			return candidates[0]
+		else:
+			raise KeyError("Key %s was not found in the database." % key)
+		
+	def write_database(self, filename, pretty=False):		
+		with open(filename, 'w') as outfile:
+			json.dump(filename, self.data)
+			
 	def write_database_csv(self, filename):
 		# Unfortunately there is no way to use element_keys as 
 		# a static method and still access keys from there. 
